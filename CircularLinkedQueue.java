5d382f44b572f3d2caac5709e074e60a8ea7185a
Ed <edoisa6@gmail.com>
2017-10-14 23:31:40 -0700

    first commit

---
+++ CircularLinkedQueue.java
@@ -0,0 +1,13 @@
+
+
+/**
+ * A class implementing a Queue using circular linked data
+ */
+
+public class CircularLinkedQueue {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+}
---
+++ LinkedQueue.java
@@ -0,0 +1,13 @@
+
+
+/**
+ * A class implementing a Queue using linked data
+ */
+
+public class LinkedQueue {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+}
