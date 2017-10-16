fae8538f791c81ad528847069cc4a448da49d46f
Ed <edoisa6@gmail.com>
2017-10-15 21:06:43 -0700

    2nd commit

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -1,30 +1,65 @@
-
+import java.util.NoSuchElementException;
 
 /**
  * A class implementing a Queue using fixed sized array
  */
 
-public class ArrayQueue {
+public class ArrayQueue<T> implements QueueInterface<T> {
+	// member variables
+	private T[] queue;
+	private static final int MAX_CAPACITY = 10;
+	private int front; // front of queue
+	private int back; // back of queue
+	
+	private EmptyQueueException emptyQueueException = new EmptyQueueException(); // exception to throw of queue is empty
+	
+	public ArrayQueue() {
+		checkCapacity();
+		
+		
+	}
 	
 	/** Adds a new entry to the back of this queue.
 	 @param newEntry  An object to be added. */
-	public void enqueue(T newEntry);
+	public void enqueue(T newEntry) {
+	
+	}
 	
 	/** Removes and returns the entry at the front of this queue.
 	 @return  The object at the front of the queue.
 	 @throws  EmptyQueueException if the queue is empty before the operation. */
-	public T dequeue();
+	public T dequeue() throws EmptyQueueException {
+	
+	}
 	
 	/**  Retrieves the entry at the front of this queue.
 	 @return  The object at the front of the queue.
 	 @throws  EmptyQueueException if the queue is empty. */
-	public T getFront();
+	public T getFront() throws EmptyQueueException {
+	
+	}
 	
 	/** Detects whether this queue is empty.
 	 @return  True if the queue is empty, or false otherwise. */
-	public boolean isEmpty();
+	public boolean isEmpty() {
+	
+	}
 	
 	/** Removes all entries from this queue. */
-	public void clear();
+	public void clear() {
+	
+	}
+	
+	/** Detects whether this queue is empty.
+	 @return  True if the queue is empty. */
+	public boolean isEmpty() {
+		return (top < 0);
+	}
+	
+	/** Check if there is room to add onto queue.
+	 @return  True if the queue has room to add. */
+	private boolean checkCapacity() {
+		return (MAX_CAPACITY - top > 0);
+	}
 	
 }
--- LinkedQueue.java
+++ LinkedQueue.java
@@ -1,13 +1,15 @@
-
+import java.util.EmptyStackException;
 
 /**
  * A class implementing a Queue using linked data
  */
 
-public class LinkedQueue {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+public class LinkedQueue<T> implements QueueInterface<T> {
+	// member variables
+	private static final int MAX_CAPACITY = 10; // default size of stack
+	private T[] stack; // actual stack that will be created
+	private int top; // top of the stack
+	
+	private EmptyStackException emptyStackException = new EmptyStackException(); // exception to throw of stack is empty
 	
 }
