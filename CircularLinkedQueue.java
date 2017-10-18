2de25930846fbaafa1a49dd28f9c1a0d62936b24
Ed <edoisa6@gmail.com>
2017-10-18 04:23:04 -0700

    done

--- CircularLinkedQueue.java
+++ CircularLinkedQueue.java
@@ -5,43 +5,44 @@
 
 public class CircularLinkedQueue<T> implements QueueInterface<T> {
 	// member variables
-//	private Node firstNode; // first node
-//	private Node emptyNode; // back node
-	
-	private Node queueNode; // References first node in queue
-	private Node freeNode;
+	private Node back; // References last node in queue
+	private Node free;
+	private final int MAX_SIZE = 10;
+	private int size;
 	
 	public CircularLinkedQueue() {
-		freeNode = new Node(null, null);
-		freeNode.setNextNode(freeNode);
-		queueNode = freeNode;
+		free = new Node(null, null);
+		free.setNextNode(free);
+		back = free;
+		size = 0;
 	}
 	
 	/** Adds a new entry to the back of this queue.
 	 @param newEntry  An object to be added. */
 	public void enqueue(T newEntry) {
-		freeNode.setData(newEntry);
+		free.setData(newEntry);
 		
 		if(isChainFull()) {
-			Node newNode = new Node(null, freeNode.getNextNode());
-			freeNode.setNextNode(newNode);
+			Node newNode = new Node(null, free.getNextNode());
+			free.setNextNode(newNode);
 		}
 		
-		freeNode = freeNode.getNextNode();
+		free = free.getNextNode();
+		size++;
 	}
 	
 	/** Removes and returns the entry at the front of this queue.
 	 @return  The object at the front of the queue.
 	 @throws EmptyQueueException if the queue is empty before the operation. */
 	public T dequeue() throws EmptyQueueException {
-		if (isChainFull())
+		if (isEmpty())
 			throw new EmptyQueueException();
 		
 		T front = getFront();
 		
-		queueNode.setData(null);
-		queueNode = queueNode.getNextNode();
+		back.setData(null);
 		
+		size--;
 		return front;
 	}
 	
@@ -52,24 +53,29 @@ public class CircularLinkedQueue<T> implements QueueInterface<T> {
 		if(isEmpty())
 			throw new EmptyQueueException();
 		
-		return queueNode.getData();
+		return back.getData();
 	}
 	
 	/** Detects whether this queue is empty.
 	 @return  True if the queue is empty, or false otherwise. */
 	public boolean isEmpty() {
-		return queueNode == freeNode;
+		return back == free;
 	}
 	
-	/**  */
+	/** Returns true when chain is full */
 	private boolean isChainFull() {
-		return queueNode == freeNode.getNextNode();
+		return (back == free.getNextNode()) || !(size <= MAX_SIZE);
 	}
 	
 	/** Removes all entries from this queue. */
 	public void clear() {
-		queueNode = null;
-		freeNode = null;
+		while (!isEmpty()) {
+			back.setData(null);
+			free = back;
+			back = free.getNextNode();
+		}
+		
+		size = 0;
 	}
 	
 	/** Node class */
@@ -86,14 +92,14 @@ public class CircularLinkedQueue<T> implements QueueInterface<T> {
 			return data;
 		}
 		
-		public void setData(T newData) {
+		void setData(T newData) {
 			data = newData;
 		}
 		
-		public Node getNextNode() {
+		Node getNextNode() {
 			return next;
 		}
-		public void setNextNode(Node newNode) {
+		void setNextNode(Node newNode) {
 			next = newNode;
 		}
 		
